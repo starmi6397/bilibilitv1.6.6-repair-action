26169d7c2871fd553b943a0b2b057e8ffbb06453
starmi6397 <122852659+starmi6397@users.noreply.github.com>
2024-08-12 13:19:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-case "\$1" in
-    *)
-        # 编译java类
-        cd mybv/java
-        ./build.sh
-        cd ../..
+# 编译java类
+cd mybv/java
+./build.sh
+cd ../..
 
-        # 编译apk
-        apktool b -c --use-aapt2 mybv
+# 编译apk
+apktool b -c --use-aapt2 mybv
 
-        # 签名apk
-        signapk platform.x509.pem platform.pk8 ./mybv/dist/mybv.apk mybv.apk
-        exit 0
-    ;;
-esac
+# 签名apk
+apksigner sign --ks platform.pk8 --out mybv.apk ./mybv/dist/mybv.apk
+exit 0
